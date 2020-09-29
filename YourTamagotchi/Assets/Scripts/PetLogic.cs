cff35fbca01bf85cb05e85bef1f1e268538440f6
Nikita-Antonov <antonovenikita@gmail.com>
2020-09-29 13:30:21 -0400

    Bars and timer
    -Addet ticker to pet to handle logic
    -Added the inputs for bars to display player stats
    -Began working on the UI
    -Added basic button

--- YourTamagotchi/Assets/Scripts/PetLogic.cs
+++ YourTamagotchi/Assets/Scripts/PetLogic.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PetLogic : MonoBehaviour
 {
@@ -26,16 +27,66 @@ public class PetLogic : MonoBehaviour
     [SerializeField] private int curFood;
 
     [SerializeField] private int curHealth;
+    [Space]
+
+    [Header("Slider Bar Reffrences")]
+    public Slider ageSlider;
+    public Slider weightSlider;
+    public Slider happinessSlider;
+    public Slider foodSlider;
+    public Slider healthSliderl;
+    [Space]
+
+    [Header("Button Reffrences")]
+    public Button feedPet;
+    public Button playWithPet;
+    
 
     private void Start()
     {
         curWeight = curHappiness = curFood = maxValue;
         curAge = 1;
+
+        //Setting all the max values of the sliders, since I am ussing whole numbers instead of floats, the numbers need to be bigger than 1
+        //Also if i wish to change the max value then they will all update
+        ageSlider.maxValue = maxValue;
+        weightSlider.maxValue = maxValue;
+        happinessSlider.maxValue = maxValue;
+        foodSlider.maxValue = maxValue;
+        healthSliderl.maxValue = maxValue;
+
+        weightSlider.value = curWeight;
+        happinessSlider.value = curHappiness;
+        foodSlider.value = curFood;
+        healthSliderl.value = maxValue;
     }
 
     private void FixedUpdate()
     {
         HealthCalculator();
+    }
+
+    void TickTimer()
+    {
+        float curTime = 10;
+        float maxTime = 10;
+
+        if (curTime > 0)
+            curTime -= Time.deltaTime;
+        if (curTime < 0)
+            curTime = 0;
+        if(curTime == 0)
+        {
+            HealthCalculator();
+
+            ageSlider.value = curAge;
+            weightSlider.value = curWeight;
+            happinessSlider.value = curHappiness;
+            foodSlider.value = curFood;
+            healthSliderl.value = maxValue;
+
+            curTime = maxTime;
+        }
     }
 
     /*
@@ -54,13 +105,15 @@ public class PetLogic : MonoBehaviour
     //Function for UI Button to feed the pet
     public void FeedPet()
     {
-
+        int petFood = 4;
+        curFood += petFood;
     }
 
     //Function for UI Button to play with the pet
     public void PlayWithPet()
     {
-
+        int happyPlay = 10;
+        curHappiness += happyPlay;
     }
 
 }
