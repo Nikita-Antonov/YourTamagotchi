a2255fb132402a627ca086ef194c4e60c63d5b65
Nikita-Antonov <antonovenikita@gmail.com>
2020-10-11 13:50:06 -0400

    Re-Making the project
    Removed the PerLogic script for now,  began to rework the game fundementaly, from a 2D tomagachi Game, to a 3D "Talking Tom" like game.

---
+++ YourTamagotchi/Assets/Scripts/Pet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+
+    public float moveSpeed = 3f;
+    public float rotSpeed = 100;
+
+    private bool isWandering = false;
+
+    private void Update()
+    {
+        
+    }
+
+    void Wander()
+    {
+
+    }
+
+}
