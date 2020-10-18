be74d845c770da6abb1bdfedd3f084c75239a08c
Nikita-Antonov <antonovenikita@gmail.com>
2020-10-18 00:00:21 -0400

    Pet Wanders
    Added the components for the pet to wander aroun in the "room", next will be implenting reactions

--- YourTamagotchi/Assets/Scripts/Pet.cs
+++ YourTamagotchi/Assets/Scripts/Pet.cs
@@ -8,16 +8,34 @@ public class Pet : MonoBehaviour
     public float moveSpeed = 3f;
     public float rotSpeed = 100;
 
-    private bool isWandering = false;
+    public Transform moveSpot;
 
-    private void Update()
-    {
-        
+    private float waitTime;
+    [SerializeField] private float startWaitTime;
+
+    public float minX, minZ, maxX, maxZ;
+
+    void Start(){
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector3(Random.Range(minX, maxX), .5f, Random.Range(minZ, maxZ));
     }
 
-    void Wander()
-    {
+    void Update(){
+        transform.position = Vector3.MoveTowards(transform.position, moveSpot.position, moveSpeed * Time.deltaTime);
 
+        if (Vector3.Distance(transform.position, moveSpot.position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                moveSpot.position = new Vector3(Random.Range(minX, maxX), .5f, Random.Range(minZ, maxZ));
+                Quaternion.LookRotation(moveSpot.position, Vector3.up);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
     }
 
 }
